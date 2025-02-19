025fc3448c213f01206c42dfed980305760b24ad
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2025-02-19 13:54:07 +0100

    Update

--- meteor_detect_class/detector_and_classification.py
+++ meteor_detect_class/detector_and_classification.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import DBSCAN
-#import orb_algo
-#from skimage.measure import label, regionprops
 
+
+# TODO VAR CONF
 def detect_and_cluster_bursts(image_path, eps=30, min_samples=5, display=True):
     # Bild einlesen und in Graustufen konvertieren
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
@@ -15,29 +15,29 @@ def detect_and_cluster_bursts(image_path, eps=30, min_samples=5, display=True):
 
     # Keypoints in (x, y)-Koordinaten extrahieren
     keypoints_coords = np.array([kp.pt for kp in keypoints])
-    image_keypoint= cv2.drawKeypoints(image,keypoints, None)
+    image_keypoint = cv2.drawKeypoints(image, keypoints, None)
     # DBSCAN Clustering anwenden
     if len(keypoints_coords) > 0:
         db = DBSCAN(eps=eps, min_samples=min_samples).fit(keypoints_coords)
         labels = db.labels_
-    else:  labels = []
+    else:
+        labels = []
 
     # Bild kopieren, um Cluster zu zeichnen
-    #clustered_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+    # clustered_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
 
-    
     # Klassifizierte Bursts sammeln
     bursts = []
-    burst_positions= []
-    
+    burst_positions = []
+
     # Klassifizierte Bursts sammeln
     critical_bursts = []
     non_critical_bursts = []
-    rect_positions= []
+    rect_positions = []
 
     # Durch alle Cluster iterieren und jedes Cluster markieren
     unique_labels = set(labels)
-    
+
     for label in unique_labels:
         if label == -1:
             # Rauschen (kein Cluster) ignorieren
@@ -45,32 +45,31 @@ def detect_and_cluster_bursts(image_path, eps=30, min_samples=5, display=True):
         cluster_points = keypoints_coords[labels == label]
         x_min, y_min = np.min(cluster_points, axis=0)
         x_max, y_max = np.max(cluster_points, axis=0)
-        #minr, minc, maxr, maxc = label.bbox
+        # minr, minc, maxr, maxc = label.bbox
         duration = x_max - x_min  # Breite in Pixeln
         # Klassifiziere basierend auf der Dauer
         if duration >= 5:  # Schwellwert fuer kritische Bursts (entspricht ca. 0.5s)
             critical_bursts.append(label)
             # Punkte im aktuellen Cluster finden
-            cluster_points= keypoints_coords[labels == label]
+            cluster_points = keypoints_coords[labels == label]
             # Begrenzungsrechteck gruen um Cluster zeichnen
             x_min, y_min = np.min(cluster_points, axis=0)
             x_max, y_max = np.max(cluster_points, axis=0)
-            rect_positions.append((x_min,y_min,x_max,y_max))
+            rect_positions.append((x_min, y_min, x_max, y_max))
 
             cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
         else:
             non_critical_bursts.append(label)
-            cluster_points= keypoints_coords[labels == label]
+            cluster_points = keypoints_coords[labels == label]
             # Begrenzungsrechteck rot um Cluster zeichnen
             x_min, y_min = np.min(cluster_points, axis=0)
             x_max, y_max = np.max(cluster_points, axis=0)
-            rect_positions.append((x_min,y_min,x_max,y_max))
+            rect_positions.append((x_min, y_min, x_max, y_max))
             cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (255, 0, 0), 2)
 
-    
     if display:
         fig, ax = plt.subplots()
-        plt.imshow(image) 
+        plt.imshow(image)
 
         ax.set_xticks(np.linspace(0, 495, 6))  # 6 Ticks
         ax.set_xticklabels(np.linspace(0, 25, 6).round(2))  # Zeit in Sekunden
@@ -81,13 +80,15 @@ def detect_and_cluster_bursts(image_path, eps=30, min_samples=5, display=True):
         plt.xlabel('Time [s]')
         plt.ylabel('Frequency [Hz]')
         plt.title('Spectrogram 25 Seconds')
-        #plt.yscale()
+        # plt.yscale()
         plt.title("Burst-Erkennung und Clusterbildung mit ORB und DBSCAN")
         plt.savefig('spectrogram2detected.jpg', format='jpg', bbox_inches='tight', pad_inches=0)
         if display:
             plt.show()
-            
+
     return bursts, unique_labels, burst_positions, critical_bursts, non_critical_bursts
+
+
 '''
 def find_matches(image_path1, image_path2):
     image1 = cv2.imread(image_path1, cv2.IMREAD_COLOR)
---
+++ meteor_detect_class/prime_detection_deprecated.py
@@ -0,0 +1,300 @@
+import pyaudio
+import wave
+import time
+import csv
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import struct
+import twitchrealtimehandler
+import detector_and_classification as detection
+import os
+
+# import twitchhandler.py
+
+# Einstellungen
+# FORMAT = pyaudio.paInt16  # Audioformat (16-bit)
+# CHANNELS = 1  # Stereo-Kanaele
+# RATE = 44100  # Abtastrate (Hz)
+# CHUNK = 1024 * 4  # Groesse der Datenbloecke
+# DURATION = 30  # Aufnahmedauer in Sekunden
+# FILENAME = "loopback_recording.wav"  # Datei, in der die Aufnahme gespeichert wird
+DISPLAY = False
+
+C_FILE_PATH_OUT = "/home/meteor/Desktop/testMSOUT/"
+C_FILE_PATH_SPEC = "/tmp/spectrogram2.jpg"
+
+time_meas_dict = {}
+
+
+def start_time_meas(idk: str):
+    time_meas_dict[idk] = datetime.now()
+
+
+def end_time_meas(idk: str):
+    # and print in seconds
+    time = datetime.now() - time_meas_dict[idk]
+    print(f"Time for {idk}: {time.total_seconds()} seconds")
+
+
+assert os.path.exists(C_FILE_PATH_OUT), f"Path not found: {C_FILE_PATH_OUT}"
+
+audio_grabber = twitchrealtimehandler.TwitchAudioGrabber(
+    twitch_url="https://www.twitch.tv/astronomiemuseum",
+    # twitch_url="https://www.twitch.tv/noway4u_sir",
+    blocking=True,  # wait until a segment is available
+    segment_length=30,  # segment length in seconds
+    rate=5000,  # sampling rate of the audio
+    channels=1,  # number of channels
+    dtype=np.int16  # quality of the audio could be [np.int16, np.int32, np.float32, np.float64]
+)
+
+
+# def load_wav(filename):
+#     with wave.open(filename, 'r') as wav_file:
+#         params = wav_file.getparams()
+#         num_channels = params.nchannels
+#         sample_width = params.sampwidth
+#         frame_rate = params.framerate
+#         num_frames = params.nframes
+#
+#         raw_data = wav_file.readframes(num_frames)
+#         total_samples = num_frames * num_channels
+#
+#         if sample_width == 2:
+#             fmt = f"{total_samples}h"
+#         else:
+#             raise ValueError("Unsupported sample width")
+#
+#         integer_data = struct.unpack(fmt, raw_data)
+#         audio_data = np.array(integer_data).reshape(-1, num_channels)
+#
+#         return audio_data, frame_rate
+
+
+def plot_spectrogram(iq_segment, fs, display=True, vmin=10, vmax=30):
+    # Um Rauschgrund zu entfernen wird die Rauschleistung berechnet:
+    NFFT = 2048
+    delta_f = fs / NFFT  # Frequenzaufloesung im Spectrogramm und fuer die Rauschberechnung
+
+    Pxx, freqs, bins, im = plt.specgram(iq_segment[:, 0], Fs=fs, NFFT=NFFT,
+                                        noverlap=NFFT // 2)  # 29 40,vmin=29, vmax=40
+    # Frequenzbereich in dem nur Rauschen vorkommt und keine Bursts
+    lower_freq = 250
+    upper_freq = 800
+    noise_band = (freqs >= lower_freq) & (freqs <= upper_freq)
+    # Bandbreite fuer das Frequenzband
+    bandwidth = np.sum(noise_band) * delta_f  # Bandbreite in Hz
+
+    # Gesamte Leistung in diesem Frequenzband
+    band_power = np.sum(Pxx[noise_band])  # Summe der Leistung im Frequenzband
+    power_density_db_hz = 10 * np.log10(band_power / bandwidth)
+    factor = 40 / 23
+
+    # db-Werte in Pxx umrechnen
+    Pxx_db = 10 * np.log10(Pxx)  # in dB
+    Pxx_db[np.isinf(Pxx_db)] = -np.inf
+
+    plt.imshow(Pxx_db, aspect='auto', origin='lower', extent=[bins[0], bins[-1], freqs[0], freqs[-1]],
+               vmin=power_density_db_hz / factor + 8, vmax=40)
+    # plt.xlabel('Time [s]')
+    # plt.ylabel('Frequency [Hz]')
+    # plt.title('Spectrogram 25 Seconds')
+    plt.ylim(800, 1200)  # Frequenzbereich von 0 bis 2000 Hz
+    # plt.colorbar(label='Leistung/Frequenz (dB/Hz)')
+    fig = plt.axis('off')
+    # plt.tight_layout()
+    plt.savefig(C_FILE_PATH_SPEC, format='jpg', bbox_inches='tight', pad_inches=0)
+    if display:
+        plt.show()
+    # plt.show(block=False)
+    del Pxx, Pxx_db, band_power
+    plt.close()
+
+
+# def list_audio_devices():
+#     # Liste alle verfuegbaren Audioeingabegeraete auf.
+#     audio = pyaudio.PyAudio()
+#     print('Verfuegbare Audiogeraete:')
+#     for i in range(audio.get_device_count()):
+#         device = audio.get_device_info_by_index(i)
+#         print(
+#             f"Index {i}: {device['name']} - {device['maxInputChannels']} Eingaenge, {device['maxOutputChannels']} Ausgaenge")
+#     audio.terminate()
+#
+#
+# def record_loopback(device_index):
+#     """Zeichnet Audio vom angegebenen Geraet auf."""
+#     audio = pyaudio.PyAudio()
+#
+#     # Oeffne den Stream
+#     stream = audio.open(format=FORMAT, channels=CHANNELS,
+#                         rate=RATE, input=True,
+#                         input_device_index=device_index,
+#                         frames_per_buffer=CHUNK)
+#
+#     # Oeffne die WAV-Datei zum Schreiben
+#     wf = wave.open(FILENAME, 'wb')
+#     wf.setnchannels(CHANNELS)
+#     wf.setsampwidth(audio.get_sample_size(FORMAT))
+#     wf.setframerate(RATE)
+#
+#     print("Aufnahme gestartet...")
+#     for _ in range(0, int(RATE / CHUNK * DURATION)):
+#         data = stream.read(CHUNK, exception_on_overflow=False)
+#         wf.writeframes(data)  # Schreibe Frames direkt in die Datei
+#     print("Aufnahme beendet.")
+#
+#     # Schliesse den Stream und die Datei
+#     stream.stop_stream()
+#     stream.close()
+#     audio.terminate()
+#     wf.close()
+#
+#     print(f"Audio erfolgreich gespeichert in {FILENAME}")
+
+
+# audio_segment = audio_grabber.grab()
+
+# Liste alle verfuegbaren Geraete
+# list_audio_devices()
+
+# Waehle den Index des Loopback-Geraets aus
+# device_index = int(input("Gib den Index des Loopback-Geraets ein: "))
+# record_loopback(device_index)
+
+# iq_data, fs = load_wav(FILENAME)
+# print(type(iq_data))
+# print(iq_data.shape)
+# print(iq_data[:10])
+# raise Exception("Stop")
+# iq_segment = iq_data
+fs = 5000
+n_critical = 0
+n_non_critical = 0
+
+start_time = datetime.now()  # Startzeit
+start_time1 = datetime.now()
+save_interval = timedelta(minutes=59.8)  # Intervall von 1h
+save_interval2 = timedelta(hours=24)  # Intervall von 24 h
+
+previous_date = datetime.now().strftime('%Y-%m-%d')
+# output_file = "values_sum.txt"  # Datei zum Speichern der Summe
+# Aktuelles Datum im Format YYYYMMDD
+date_string = datetime.now().strftime("%Y%m%d")
+separator = ";"
+# Dateiname basierend auf dem aktuellen Datum
+file_name = f"{C_FILE_PATH_OUT}{date_string}.csv"
+columns = ["Timestamp", "Anzahl", "Kritisch"]
+df = pd.DataFrame(columns=columns)
+# Pruefen, ob die Datei bereits existiert
+if not os.path.exists(file_name):
+    # Datei existiert nicht, also erstellen
+    df.to_csv(file_name, sep=separator, index=False)
+    print(f"Datei {file_name} wurde erstellt.")
+else:
+    print(f"Datei {file_name} existiert bereits.")
+
+#
+
+while True:
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    print(f"Startzeit: {start_time}\n")
+
+    # Schritt 1: Audiosegment erfassen
+    start_time_meas("grab_audio")
+    try:
+        print("Starte Audioaufnahme...")
+        audio_segment = audio_grabber.grab()
+        print("Audioaufnahme abgeschlossen.")
+    except Exception as e:
+        print(f"Fehler bei der Audioaufnahme: {e}")
+        time.sleep(5)  # Wartezeit bei Fehlern
+        continue
+    print(f"Audiosegment Größe: {audio_segment.shape}")
+    if audio_segment.shape[0] != 150000:
+        print("Fehler: Das Audiosegment ist leer.")
+        audio_grabber.terminate()
+        # time.sleep(10)
+        audio_grabber = twitchrealtimehandler.TwitchAudioGrabber(
+            twitch_url="https://www.twitch.tv/astronomiemuseum",
+            blocking=True,  # wait until a segment is available
+            segment_length=30,  # segment length in seconds
+            rate=5000,  # sampling rate of the audio
+            channels=1,  # number of channels
+            dtype=np.int16  # quality of the audio could be [np.int16, np.int32, np.float32, np.float64]
+        )
+        continue
+    end_time_meas("grab_audio")
+
+    # Schritt 2: Spektrogramm plotten
+    start_time_meas("plot_spectrogram")
+    print("Erstelle Spektrogramm...")
+    plot_spectrogram(audio_segment, fs, DISPLAY)
+    print("Spektrogramm wurde erstellt.")
+
+    # audio_grabber.terminate()
+    # print("Audioaufnahme beendet.\n")
+    del audio_segment
+    end_time_meas("plot_spectrogram")
+
+    # Schritt 3: Burst-Erkennung und Clusterbildung
+    start_time_meas("detect_and_cluster_bursts")
+    image_path1 = C_FILE_PATH_SPEC
+    print("Starte Burst-Erkennung und Clusterbildung...")
+    bursts, unique_labels, burst_positions, critical_bursts, non_critical_bursts = detection.detect_and_cluster_bursts(
+        image_path1, display=DISPLAY)
+    print("Burst-Erkennung und Clusterbildung abgeschlossen.")
+    end_time_meas("detect_and_cluster_bursts")
+
+    # Schritt 4: Ergebnisse aktualisieren
+    n_critical += len(critical_bursts)
+    n_non_critical += len(non_critical_bursts)
+    print(f"Anzahl kritischer Bursts in dieser Stunde: {n_critical}")
+    print(f"Anzahl nicht kritischer Bursts in dieser Stunde: {n_non_critical}\n")
+
+    # Schritt 5: Ueberpruefen, ob 1 Stunden vergangen ist
+    if datetime.now() - start_time >= save_interval:
+        print("Speichere Ergebnisse...")
+        # Zeitstempel für die CSV-Datei ohne Millisekunden formatieren
+        formatted_timestamp = start_time.strftime("%Y-%m-%d %H:%M:%S")
+        new_row = {"Timestamp": formatted_timestamp, "Anzahl": n_critical + n_non_critical, "Kritisch": n_critical}
+
+        # Bestehende Datei einlesen
+        df2 = pd.read_csv(file_name, sep=separator)
+
+        # Neue Zeile als DataFrame erstellen
+        new_row_df = pd.DataFrame([new_row])
+
+        # Zusammenfügen des bestehenden DataFrames und der neuen Zeile
+        df2 = pd.concat([df2, new_row_df], ignore_index=True)  # Hier wird `pd.concat` verwendet
+
+        # DataFrame speichern
+        df2.to_csv(file_name, sep=separator, index=False)
+
+        print(f"Neue Zeile hinzugefügt: {start_time}")
+
+        # Variablen zurücksetzen
+        n_critical = 0
+        n_non_critical = 0
+        start_time = datetime.now()
+
+        # Schritt 6: Ueberpruefen, ob 24 Stunden vergangen sind
+    if current_date != previous_date:
+        previous_date = current_date
+        print("Neuer Tag, lege neues csv-file an...")
+        # Aktuelles Datum im Format YYYYMMDD
+        date_string = datetime.now().strftime("%Y%m%d")
+        # Dateiname basierend auf dem aktuellen Datum
+        file_name = f"{C_FILE_PATH_OUT}{date_string}.csv"
+        columns = ["Timestamp", "Anzahl", "Kritisch"]
+        df = pd.DataFrame(columns=columns)
+        df.to_csv(file_name, sep=separator, index=False)
+        print(f"Die CSV-Datei '{file_name}' wurde erfolgreich erstellt.")
+        # Zurücksetzen der Summen und Aktualisieren der Startzeit
+        n_critical = 0
+        n_non_critical = 0
+        start_time1 = datetime.now()
+        print("Summen zurückgesetzt und Startzeit aktualisiert.\n")

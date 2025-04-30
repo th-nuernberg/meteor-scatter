a6bc10766a0fdf7baef7891683ea2a9703ccaf92
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2025-04-30 11:18:52 +0200

    Update

--- meteor_detect_class/prime_watchdog.sh
+++ meteor_detect_class/prime_watchdog.sh
@@ -1,4 +1,21 @@
-# PLS ACTIVE VIRTUAL ENVIRONMENT BEFORE RUNNING THIS SCRIPT
+# PLS ACTIVE VIRTUAL ENVIRONMENT BEFORE RUNNING THIS SCRIPT (NON DOCKER)
+
+DOCKER_MODE=false
+
+for arg in "$@"; do
+    if [ "$arg" == "--docker" ]; then
+        DOCKER_MODE=true
+    fi
+done
+
+if [ "$DOCKER_MODE" == true ]; then
+    echo "Docker-Modus ist aktiviert"
+    LOG_FILE_PATH="/home/meteor/Documents/meteor-detection/log-out/log.txt"
+    pip freeze > /home/meteor/Documents/meteor-detection/log-out/requirements-backup.txt
+else
+    echo "Docker-Modus ist deaktiviert"
+    LOG_FILE_PATH="log.txt"
+fi
 
 while true; do
 
@@ -7,10 +24,9 @@ while true; do
     echo
     echo "RESTART PRIME DETECTION at $current_time"
 
-    echo "\n" >> log.txt
-    echo "RESTART PRIME DETECTION at $current_time" >> log.txt
-    python prime_detection.py >> log.txt
-
+    echo "\n" >> $LOG_FILE_PATH
+    echo "RESTART PRIME DETECTION at $current_time" >> $LOG_FILE_PATH
+    python prime_detection.py >> $LOG_FILE_PATH 2>&1
 
     current_time=$(date "+%Y.%m.%d-%H.%M.%S")
     echo "CRASHED... at $current_time"
